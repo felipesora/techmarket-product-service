f0c20b9ed0db1f8b8c8654f296e5b2036fd7156c
felipesora <felipeusora@gmail.com>
2026-03-12 14:08:57 -0300

    criando microsservico de produtos

---
+++ src/main/java/br/com/techmarket_product_service/TechmarketProductServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.techmarket_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmarketProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmarketProductServiceApplication.class, args);
+	}
+
+}
