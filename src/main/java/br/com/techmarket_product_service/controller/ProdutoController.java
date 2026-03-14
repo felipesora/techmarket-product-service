1274f91282f38afeb4cab951df40f1695efb9a69
felipesora <felipeusora@gmail.com>
2026-03-14 16:34:04 -0300

    feat: enviando produto cadastrado para fila de produtos cadastrados

--- src/main/java/br/com/techmarket_product_service/controller/ProdutoController.java
+++ src/main/java/br/com/techmarket_product_service/controller/ProdutoController.java
@@ -3,9 +3,11 @@ package br.com.techmarket_product_service.controller;
 import br.com.techmarket_product_service.dto.produto.ProdutoCreateDTO;
 import br.com.techmarket_product_service.dto.produto.ProdutoResponseDTO;
 import br.com.techmarket_product_service.dto.produto.ProdutoUpdateDTO;
+import br.com.techmarket_product_service.dto.produtoSnapshot.ProdutoSnapshotDTO;
 import br.com.techmarket_product_service.service.ProdutoService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -21,8 +23,11 @@ public class ProdutoController {
 
     private final ProdutoService produtoService;
 
-    public ProdutoController(ProdutoService produtoService) {
+    private final RabbitTemplate rabbitTemplate;
+
+    public ProdutoController(ProdutoService produtoService, RabbitTemplate rabbitTemplate) {
         this.produtoService = produtoService;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @GetMapping
@@ -41,6 +46,11 @@ public class ProdutoController {
     public ResponseEntity<ProdutoResponseDTO> cadastrarUsuario(@RequestBody @Valid ProdutoCreateDTO dto, UriComponentsBuilder uriBuilder) {
         ProdutoResponseDTO produto = produtoService.cadastrarProduto(dto);
         URI endereco = uriBuilder.path("/produtos/{id}").buildAndExpand(produto.id()).toUri();
+
+        ProdutoSnapshotDTO produtoSnapshotDTO = converterParaProdutoSnapshot(produto);
+        System.out.println("Enviando produto: " + produtoSnapshotDTO);
+        rabbitTemplate.convertAndSend("produto.exchange", "", produtoSnapshotDTO);
+
         return ResponseEntity.created(endereco).body(produto);
     }
 
@@ -54,5 +64,16 @@ public class ProdutoController {
     public ResponseEntity<ProdutoResponseDTO> remover(@PathVariable @NotNull String id) {
         produtoService.deletarProduto(id);
         return ResponseEntity.noContent().build();
+    }
+
+    private ProdutoSnapshotDTO converterParaProdutoSnapshot(ProdutoResponseDTO produtoResponseDTO) {
+        return new ProdutoSnapshotDTO(
+                produtoResponseDTO.id(),
+                produtoResponseDTO.codigo(),
+                produtoResponseDTO.nome(),
+                produtoResponseDTO.preco(),
+                produtoResponseDTO.estoque(),
+                produtoResponseDTO.status()
+        );
     }
 }
---
+++ src/main/java/br/com/techmarket_product_service/dto/produtoSnapshot/ProdutoSnapshotDTO.java
@@ -0,0 +1,18 @@
+package br.com.techmarket_product_service.dto.produtoSnapshot;
+
+import br.com.techmarket_product_service.model.enums.StatusProduto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+
+public record ProdutoSnapshotDTO(
+        @JsonProperty("id_mongo")
+        String idMongo,
+        String codigo,
+        String nome,
+        @JsonProperty("preco_unitario")
+        BigDecimal precoUnitario,
+        Integer estoque,
+        StatusProduto status
+) {
+}
