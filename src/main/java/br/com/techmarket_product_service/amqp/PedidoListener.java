12d200ef7643501a0b6c4e180593bc9e1e6bd7ec
felipesora <felipeusora@gmail.com>
2026-03-18 00:01:38 -0300

    refactor: refatorando PedidoListener

--- src/main/java/br/com/techmarket_product_service/amqp/PedidoListener.java
+++ src/main/java/br/com/techmarket_product_service/amqp/PedidoListener.java
@@ -1,10 +1,14 @@
 package br.com.techmarket_product_service.amqp;
 
 import br.com.techmarket_product_service.dto.pedido.ItemPedidoEventDTO;
+import br.com.techmarket_product_service.dto.pedido.PedidoCanceladoEventDTO;
 import br.com.techmarket_product_service.dto.pedido.PedidoCriadoEventDTO;
 import br.com.techmarket_product_service.service.ProdutoService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.BiConsumer;
 
 @Component
 public class PedidoListener {
@@ -20,19 +24,26 @@ public class PedidoListener {
         System.out.println("Mensagem recebida da fila de pedidos criados");
         System.out.println("Conteúdo: " + evento);
 
-        for (ItemPedidoEventDTO item: evento.itens()) {
-            produtoService.baixarEstoque(item.produtoIdMongo(), item.quantidade());
-            System.out.println("Estoque do produto " + item.produtoIdMongo() + " atualizado!");
-        }
+        processarItens(evento.itens(), true);
     }
 
     @RabbitListener(queues = "pedido.cancelado")
-    public void recebePedidoCancelado(PedidoCriadoEventDTO evento) {
+    public void recebePedidoCancelado(PedidoCanceladoEventDTO evento) {
         System.out.println("Mensagem recebida da fila de pedidos cancelados");
         System.out.println("Conteúdo: " + evento);
 
-        for (ItemPedidoEventDTO item: evento.itens()) {
-            produtoService.devolverEstoque(item.produtoIdMongo(), item.quantidade());
+        processarItens(evento.itens(), false);
+    }
+
+    private void processarItens(List<ItemPedidoEventDTO> itens, boolean baixarEstoque) {
+        for (ItemPedidoEventDTO item: itens) {
+
+            if (baixarEstoque) {
+                produtoService.baixarEstoque(item.produtoIdMongo(), item.quantidade());
+            } else {
+                produtoService.devolverEstoque(item.produtoIdMongo(), item.quantidade());
+            }
+
             System.out.println("Estoque do produto " + item.produtoIdMongo() + " atualizado!");
         }
     }
