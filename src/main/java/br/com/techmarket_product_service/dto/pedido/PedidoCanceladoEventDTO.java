d6f2de7546e20ad8bc6190ec5b5cecff3d61f949
felipesora <felipeusora@gmail.com>
2026-03-18 15:57:07 -0300

    feat: ajustando dtos de pedidoEvent

--- src/main/java/br/com/techmarket_product_service/dto/pedido/PedidoCanceladoEventDTO.java
+++ src/main/java/br/com/techmarket_product_service/dto/pedido/PedidoCanceladoEventDTO.java
@@ -1,9 +1,14 @@
 package br.com.techmarket_product_service.dto.pedido;
 
+import br.com.techmarket_product_service.model.enums.MetodoPagamentoPedido;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 public record PedidoCanceladoEventDTO (
         Long pedidoId,
+        BigDecimal valorTotal,
+        MetodoPagamentoPedido metodoPagamento,
         List<ItemPedidoEventDTO> itens
 ) {
 }
---
+++ src/main/java/br/com/techmarket_product_service/model/enums/MetodoPagamentoPedido.java
@@ -0,0 +1,7 @@
+package br.com.techmarket_product_service.model.enums;
+
+public enum MetodoPagamentoPedido {
+    PIX,
+    CARTAO_CREDITO,
+    BOLETO
+}
