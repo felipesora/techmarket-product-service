f90cb2cf5f9eb0870699d293e3c572d10edab65d
felipesora <felipeusora@gmail.com>
2026-03-31 16:01:35 -0300

    feat: requisições de buscar por busca e categoria

--- src/main/java/br/com/techmarket_product_service/repository/ProdutoRepository.java
+++ src/main/java/br/com/techmarket_product_service/repository/ProdutoRepository.java
@@ -1,7 +1,14 @@
 package br.com.techmarket_product_service.repository;
 
 import br.com.techmarket_product_service.model.Produto;
+import br.com.techmarket_product_service.model.enums.CategoriaProduto;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends MongoRepository<Produto, String> {
+    List<Produto> findByCategoria(CategoriaProduto categoria, Sort sort);
+
+    List<Produto> findByNomeContainingIgnoreCase(String nome, Sort sort);
 }
--- src/main/java/br/com/techmarket_product_service/service/ProdutoService.java
+++ src/main/java/br/com/techmarket_product_service/service/ProdutoService.java
@@ -8,11 +8,13 @@ import br.com.techmarket_product_service.exception.EntityNotFoundException;
 import br.com.techmarket_product_service.exception.RegraNegocioException;
 import br.com.techmarket_product_service.mapper.ProdutoMapper;
 import br.com.techmarket_product_service.model.Produto;
+import br.com.techmarket_product_service.model.enums.CategoriaProduto;
 import br.com.techmarket_product_service.model.enums.StatusProduto;
 import br.com.techmarket_product_service.repository.ProdutoRepository;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,6 +42,44 @@ public class ProdutoService {
     public List<ProdutoResponseDTO> obterProdutosFavoritos(List<String> ids) {
         return produtoRepository
                 .findAllById(ids)
+                .stream()
+                .map(ProdutoMapper::converterParaResponseDTO)
+                .toList();
+    }
+
+    public List<ProdutoResponseDTO> obterProdutosPorCategoria(String categoria, String ordenarPor) {
+        CategoriaProduto categoriaProdutoEnum = CategoriaProduto.valueOf(categoria.toUpperCase());
+        Sort sort = Sort.unsorted();
+
+        if (ordenarPor != null) {
+            sort = switch (ordenarPor) {
+                case "preco_asc" -> Sort.by("preco").ascending();
+                case "preco_desc" -> Sort.by("preco").descending();
+                default -> sort;
+            };
+        }
+
+        return produtoRepository
+                .findByCategoria(categoriaProdutoEnum, sort)
+                .stream()
+                .map(ProdutoMapper::converterParaResponseDTO)
+                .toList();
+    }
+
+    public List<ProdutoResponseDTO> buscarProdutosPorNome(String busca, String ordenarPor) {
+
+        Sort sort = Sort.unsorted();
+
+        if (ordenarPor != null) {
+            sort = switch (ordenarPor) {
+                case "preco_asc" -> Sort.by("preco").ascending();
+                case "preco_desc" -> Sort.by("preco").descending();
+                default -> sort;
+            };
+        }
+
+        return produtoRepository
+                .findByNomeContainingIgnoreCase(busca, sort)
                 .stream()
                 .map(ProdutoMapper::converterParaResponseDTO)
                 .toList();
